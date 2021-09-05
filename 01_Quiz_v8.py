fd7d108ce58472df3d121b2de64cf0b2fb2dd65b
yij-ctrl <yij@middleton.school.nz>
2021-09-05 18:52:43 +1200

    Create 01_Quiz_v8.py

---
+++ 01_Quiz_v8.py
@@ -0,0 +1,53 @@
+"""Program that runs a quiz based on capitals. It asks the users what the capital of a certain country is.
+This version imports an updated version of the list of capitals/countries.
+Jung Woo Yi
+Version 8 – 06/08/2021
+"""
+
+# Importing
+import random
+from capitals_list_v2 import capitals  # Importing the list of capitals from capitals_list
+
+answered = 0  # This is the number of questions answered
+correct = 0  # This is the number of questions that were answered correctly
+
+get_country = random.randint(0, len(capitals) - 1)  # This gets a list from within the whole list
+country = capitals[get_country][1]  # This gets the country from the list
+capital = capitals[get_country][0]  # This gets the respective capital for the country in the line above
+
+question = ""
+while question != "Q" and question != "q":
+    # Asks the user the capital of the country
+    question = input("What is the capital of {}? ".format(country))
+    if question == capital:
+        correct += 1
+        answered += 1
+        print("Correct\n")  # Message displayed when the answer is right
+        if len(capitals) > 0:
+            capitals.pop(get_country)
+        if len(capitals) > 0:
+            get_country = random.randint(0, len(capitals) - 1)  # This gets a list from within the whole list
+            country = capitals[get_country][1]  # This gets the country from the list
+            capital = capitals[get_country][0]  # This gets the respective capital for the country in the line above
+        else:
+            print("Finished")
+            break
+    elif question == "Q" or question == "q":
+        # Displaying statistics
+        print("\nNumber of questions answered correctly: {}\n"
+              "Number of questions answered: {}\n"
+              "Percentage: {:.2f}% (2 d. p.)".format(correct, answered, correct / answered * 100))
+        break
+    else:
+        answered += 1
+        print("Incorrect\n"
+              "The correct answer was {}\n".format(capital))  # Message displayed when the answer is wrong
+        if len(capitals) > 0:
+            capitals.pop(get_country)
+        if len(capitals) > 0:
+            get_country = random.randint(0, len(capitals) - 1)  # This gets a list from within the whole list
+            country = capitals[get_country][1]  # This gets the country from the list
+            capital = capitals[get_country][0]  # This gets the respective capital for the country in the line above
+        elif len(capitals) == 0:
+            print("Finished")
+            break
