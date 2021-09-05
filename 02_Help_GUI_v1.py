ca1435a59b9a104a1411f15631c416cc8de8587f
yij-ctrl <yij@middleton.school.nz>
2021-09-05 19:07:34 +1200

    Update 02_Help_GUI_v1.py

--- 02_Help_GUI_v1.py
+++ 02_Help_GUI_v1.py
@@ -3,6 +3,7 @@ Jung Woo Yi
 Version 1 – 16/08/2021
 """
 
+# Importing
 from tkinter import *
 import random
 
@@ -10,9 +11,13 @@ import random
 class Quiz:
     def __init__(self):
         print("Hey")
-        background_colour = "green"
+        background_colour = "green"  # Setting the background colour
+
+        # Creating a frame
         self.capital_quiz_frame = Frame(width=300, height=300, bg=background_colour)
         self.capital_quiz_frame.grid()
+
+        # Creating a label to display text and formatting it
         self.capital_quiz_label = Label(text="Capitals Quiz",
                                         font=("Arial", "16", "bold"),
                                         bg=background_colour,
