b2f44fca372ffa0434a08febcec9e6188d5d7834
yij-ctrl <yij@middleton.school.nz>
2021-09-06 10:04:47 +1200

    Update 05_Filename_Checking_Saving_v2.py

---
+++ 05_Filename_Checking_Saving_v2.py
@@ -0,0 +1,16 @@
+"""Program that creates a file from the user's input with information in the list.
+This version adds a '.txt' extension to the filename.
+Jung Woo Yi
+Version 2 – 31/08/2021
+"""
+
+# List that holds information
+information = ['Qwerty', 'Hello', 'Hi', 'Temperature']
+
+filename = input("Filename: ")  # The user inputs a filename
+filename = filename + ".txt"  # Adds an extension
+file = open(filename, "w+")
+
+for i in information:
+    file.write(i)
+file.close()
