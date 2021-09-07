4f570fa04c94bf152f69c368c2e297ca9944c228
yij-ctrl <yij@middleton.school.nz>
2021-09-07 21:51:22 +1200

    Create 04_Storing_History_v2.py

---
+++ 04_Storing_History_v2.py
@@ -0,0 +1,25 @@
+"""Program that stores history and prints all of the items entered and displays the most recent three items.
+This version allows the user to input as many items as they want compared to a limit of five in version 1.
+Jung Woo Yi
+Version 2 – 07/09/2021
+"""
+
+# List to store inputs
+inputs_list = []
+
+# Inputting items
+item = ""
+while item != "q":
+    item = input("Enter item: ")
+    if item == "q":  # The user can enter 'q' to quit
+        break
+    inputs_list.append(item)
+
+inputs_list.reverse()
+print("\nALL ITEMS\n")
+print(inputs_list)  # Prints all 5 items that were inputted
+print("\nLAST 3 ITEMS\n")
+
+# Prints last 3 items
+for i in range(0, 3):
+    print(inputs_list[i])
